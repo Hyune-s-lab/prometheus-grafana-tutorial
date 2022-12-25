1323ede5c86e517371da40227d80494432d262a1
Hyune-c <choi8608@gmail.com>
2022-12-25 22:34:52 +0900

    🔧 project init

---
+++ src/main/kotlin/com/example/prometheusgrafanatutorial/PrometheusGrafanaTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.prometheusgrafanatutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrometheusGrafanaTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrometheusGrafanaTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/prometheusgrafanatutorial/PrometheusGrafanaTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.prometheusgrafanatutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrometheusGrafanaTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
